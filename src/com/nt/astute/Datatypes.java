9e7c9c2c6548bae115524ee9eddb6327613c06a4
knagendrudu <knagendra529@DESKTOP-NG25TIN>
2019-12-03 23:45:43 +0530

    hello world

---
+++ src/com/nt/astute/Datatypes.java
@@ -0,0 +1,12 @@
+package com.nt.astute;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		int i=10;
+		float f=10.1f;
+		System.out.println(i);
+		System.out.println(f);
+	}
+
+}
